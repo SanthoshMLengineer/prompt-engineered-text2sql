d2780a81a4e14417e4d5044ce22f68ffc70161b0
Santhosh Kumar K S <santhoshkumarks922@gmail.com>
2026-05-23 09:44:15 +0530

    Changed host and port number

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def chat():
     return render_template_string(HTML_TEMPLATE, messages=messages)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
